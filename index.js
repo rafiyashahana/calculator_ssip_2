5bd9feafa632e37527a4de7d5b2831d40d3ee522
shahana <rafiyashahana@gmail.com>
2023-12-03 11:35:08 +0300

    update/ script

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
+const buttons = document.querySelectorAll(".btn");
 const result = document.querySelector(".result");
-const buttons = document.querySelectorAll("button");
-const op = document.getElementsByClassName("btn-calc");
 
-let str = "";
+const op = ["+", "-", "*", "/", "%", "="];
+
+let output = "";
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    if (
-      e.target.classList[1] == "btn-num" ||
-      e.target.classList[1] == "btn-op"
-    ) {
-      str += e.target.dataset.value;
-      result.innerHTML = str;
-      console.log(str);
-    } else if (e.target.dataset.value == "AC") {
-      str = "";
-      result.innerHTML = 0;
-    } else if (e.target.classList[1] == "equals") {
-      str = eval(str);
-      result.innerHTML = str;
-      console.log(str);
-    }
-    // else if ((e.target.dataset.value = "toggle")) {
-    //   str = -str;
-    //   result.innerHTML = str;
-    // }
-    else if ((e.target.dataset.value = "%")) {
-      str = str / 100;
-      result.innerHTML = str;
+    btnVal = e.target.dataset.value;
+
+    if (btnVal === "=" && output != "") {
+      output = eval(output.replace("%", "/100"));
+    } else if (btnVal === "AC") {
+      output = "";
     } else {
-      result.innerHTML = "Error";
+      if (output === "" && op.includes(btnVal)) return;
+      output += btnVal;
     }
+    result.innerHTML = output;
   });
 });
